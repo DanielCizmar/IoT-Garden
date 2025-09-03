f46d58e3ffdb638b6c9ca693a4268d59b0f1c3ce
Daniel <danielcizmar3012@gmail.com>
2025-09-03 22:56:00 +0200

    working led strip code but need thicker cables for it to work, code is temporary

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,56 @@
 // https://www.nextpcb.com/blog/ultimate-guide-to-connecting-led-light-strips-to-arduino
 // https://lastminuteengineers.com/esp32-wled-tutorial/
 
-#define LED_PIN 4   // change this to the GPIO pin you’re using
-#define LED_COUNT 30
+// Define pin and number of LEDs
+#define LED_PIN 5        // Pin connected to the LED strip
+#define NUM_LEDS 30      // Number of LEDs in your strip
 
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Create NeoPixel object
+Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-uint32_t Wheel(uint8_t WheelPos);
-void rainbowCycle(uint8_t wait);
-void colorWipe(uint32_t color, uint8_t wait);
-void theaterChase(uint32_t color, uint8_t wait);
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Generate rainbow colors
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+// Rainbow cycle across entire strip
+void rainbowCycle(int wait) {
+  uint16_t i, j;
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i * 256 / strip.numPixels() + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+
 
 void setup() {
+  
   Serial.begin(9600);
   initSensors();
 
-  strip.begin();           // Initialize strip
-  strip.show();            // Turn OFF all LEDs at start
-  strip.setBrightness(50); // Adjust brightness (0-255)
+  strip.begin();           // Initialize NeoPixel
+  strip.show();            // Turn OFF all LEDs at startup
+  strip.setBrightness(50); // Brightness (0-255)
 }
 
 void loop() {
@@ -30,34 +63,5 @@ void loop() {
   displayBME280Data();
   Serial.println("----------------------");
   delay(2000);*/
-
-  rainbowCycle(5);
-}
-
-// Helper function: rainbow colors across all pixels
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors
-    for (i = 0; i < strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Input a value 0 to 255 to get a color value
-// The colours are a transition r - g - b - back to r
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  rainbowCycle(10);
 }
